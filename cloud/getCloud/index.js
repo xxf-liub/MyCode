2e52d1fe486081d87950beae606b11c32a8a31c9
unknown <unknown@example.com>
2020-08-29 18:32:03 +0800

    5.22
    备份

--- cloud/getCloud/index.js
+++ cloud/getCloud/index.js
@@ -5,5 +5,6 @@ cloud.init()
 const MAX_LIMIT = 12
 // 云函数入口函数
 exports.main = async (event, context) => {
+  
   return cloud.database().collection('list').skip(event.page * MAX_LIMIT).limit(MAX_LIMIT).orderBy('zan', 'desc').get()
 }
